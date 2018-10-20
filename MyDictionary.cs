74e1481577b5095e1eea8410418f043e1b83d212
Jonathan Hayes <jonathan.hayes32@outlook.com>
2018-10-20 17:23:26 -0400

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseKeyValues
+{
+    class MyDictionary
+    {
+        KeyValue[] Array = new KeyValue[60];
+        private int Counter = 1;
+        public Object this[string Key1]
+        {
+            get
+            {
+                if (Counter < 1)
+                {
+                    throw new KeyNotFoundException();
+                }
+                for (int i = 0; i < Array.Length; i++)
+                {
+                    if (Array[i].Key == Key1)
+                    {
+                        return Array[i].Value;
+                    }
+                }
+                throw new KeyNotFoundException();
+            }
+            set
+            {
+                bool isRelevant = false;
+                for (int i = 0; i < Counter; i++)
+                {
+                    if (Array[i].Key == Key1)
+                    {
+                        Array[i] = new KeyValue(Key1, Array[i].Value);
+                        isRelevant = true;
+                    }
+                }
+                if (!isRelevant)
+                {
+                    Array[Counter + 1] = new KeyValue(Key1, value);
+                }
+                Counter++;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseKeyValues
+{
+    public struct KeyValue
+    {
+        public string Key;
+        public object Value;
+        
+        public KeyValue(string Key, object Value)
+        {
+            this.Key = Key;
+            this.Value = Value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new MyDictionary();
+            try
+            {
+                Console.WriteLine(d["Cats"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            d["Cats"] = 42;
+            d["Dogs"] = 17;
+            Console.WriteLine($"{(int)d["Cats"]}, { (int)d["Dogs"]}");
+        }
+    }
+}
